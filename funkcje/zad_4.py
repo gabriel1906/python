38c23026687bc6bd007784e6a2beb983a3d061d5
gabriel1906 <gabriel.sieradzki@gmail.com>
2018-10-21 12:17:14 +0200

    niedziela

---
+++ funkcje/zad_4.py
@@ -0,0 +1,16 @@
+
+def naleznosc(a,b):
+    a = 10
+    b = 2.5
+return (a + b)
+
+
+def do_zaplaty(naleznosc):
+    out = f"""Cena za kg: {a}
+    Waga: {b}
+    Należność wynosi: {naleznosc}
+    """
+print (naleznosc)
+
+#print("należność wynosi:", naleznosc)
+
